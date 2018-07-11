14a86aae75c6b22d68e51547117e5906bf59b7c5
GabrielFSilva <gabrielsilva.prog@gmail.com>
2018-07-10 23:31:54 -0300

    Base Project
    Mecânicas básicas do Level 3

---
+++ Nina/Assets/Scripts/HeadgearSpriteManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeadgearSpriteManager : MonoBehaviour
+{
+    public enum HeadAcessoryType
+    {
+        NOTHING,
+        BLUE,
+        RED,
+        GREEN
+    }
+
+    public int activeHeadgearIndex;
+    public List<Image> headgearImages;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeHeadgear(int direction)
+    {
+        headgearImages[activeHeadgearIndex].gameObject.SetActive(false);
+        activeHeadgearIndex += direction;
+
+        if (activeHeadgearIndex < 0)
+            activeHeadgearIndex = headgearImages.Count - 1;
+        else if (activeHeadgearIndex == headgearImages.Count)
+            activeHeadgearIndex = 0;
+
+        headgearImages[activeHeadgearIndex].gameObject.SetActive(true);
+    }
+}
